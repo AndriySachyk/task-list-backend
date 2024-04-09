9b6b037d14b29c789bb9dc5500afc0b215a0ef36
AndriySachyk <asachyk1312@gmail.com>
2024-04-09 23:30:42 +0300

    Update

--- src/cors.config.ts
+++ src/cors.config.ts
@@ -1,10 +1,10 @@
 // cors.config.ts
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-export const corsOptions: CorsOptions = {
-  origin: true, // або вкажіть конкретні домени, які мають доступ, наприклад ['http://localhost:3000']
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type, Accept',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
+// export const corsOptions: CorsOptions = {
+//   origin: true, // або вкажіть конкретні домени, які мають доступ, наприклад ['http://localhost:3000']
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   allowedHeaders: 'Content-Type, Accept',
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { corsOptions } from './cors.config';
+// import { corsOptions } from './cors.config';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors(corsOptions);
+  // app.enableCors(corsOptions);
+  app.use(cors());
+  
   app.setGlobalPrefix("/api")
   
-  await app.listen(5555,'0.0.0.0');
+  await app.listen(5555);
 }
 bootstrap();
